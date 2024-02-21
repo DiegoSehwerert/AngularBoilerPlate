6497bd1e7a8bdbded6e9a03612f5303a1b4fbe9e
DiegoSehwerert <dsehwerert@gmail.com>
2024-02-21 18:10:01 +0100

    Hciendo lo de las rutas y 1er trry de hacer un crud

---
+++ angular-boilerplate/src/app/crud/crud.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormComponent } from '../form/form.component';
+import { TableComponent } from '../table/table.component';
+
+@Component({
+  selector: 'app-crud',
+  standalone: true,
+  imports: [FormComponent, TableComponent],
+  templateUrl: './crud.component.html',
+  styleUrl: './crud.component.css'
+})
+export class CrudComponent {
+
+}
