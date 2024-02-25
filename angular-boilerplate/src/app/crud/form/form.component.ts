744f90a153909e26bca574bea55cff89497d7340
DiegoSehwerert <dsehwerert@gmail.com>
2024-02-25 21:08:54 +0100

    Actualización de archivos en el proyecto Angular, añadidos tabs funcionales que no reservan la informacion !!!cambiar

--- angular-boilerplate/src/app/crud/form/form.component.ts
+++ angular-boilerplate/src/app/crud/form/form.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
 export class FormComponent {
+  languages = ['ES', 'EN']; // Aquí se define el array de idiomas
+  selectedLanguage = 0; // Aquí se define el idioma seleccionado por defecto (el primer idioma en el array)
 
+  form = new FormGroup({
+    languageES: new FormControl(''),
+    descriptionES: new FormControl(''),
+    languageEN: new FormControl(''),
+    descriptionEN: new FormControl('')
+  });
+
+  selectLanguage(index: number) {
+    this.selectedLanguage = index; // Cambia el idioma seleccionado al que el usuario ha hecho clic
+  }
 }
--- angular-boilerplate/src/main.ts
+++ angular-boilerplate/src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { CommonModule } from '@angular/common';
+import { importProvidersFrom } from '@angular/core';
+
+
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(CommonModule)],
+});
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
