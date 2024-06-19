269d5c79ed387dcd897bf1894932938c5872e43c
DiegoSehwerert <dsehwerert@gmail.com>
2024-06-19 10:36:02 +0200

    re construction of the project

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent, title: 'Inicio' },
-  { path: 'usuarios', component: UsersComponent, title: 'Usuarios' }
-];
